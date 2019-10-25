9295666d8428b380db014587d4508a5607d6a6c1
Michael J Peppers <mjpeppersdev@protonmail.com>
2019-10-24 17:28:25 -0700

    Update app.js

--- app.js
+++ app.js
@@ -1,23 +1,27 @@
-$(function() {
-    $(window).resize(function() {
-      var originalHeight, newHeight, originalWidth, newWidth, screenHeight, screenWidth;
-      originalHeight = 480;
-      originalWidth = 852;
-      screenHeight = $(window).height();
-      screenWidth = $(window).width();
-  
-      if (originalWidth / screenWidth < originalHeight / screenHeight) {
-        newWidth = screenWidth;
-        newHeight = screenWidth / originalWidth * originalHeight;
-      } else {
-        newHeight = screenHeight;
-        newWidth = screenHeight / originalHeight * originalWidth;
-      }
-  
-      return $("video").css({
-        height: newHeight,
-        width: newWidth
-      });
-    });
-    $(window).resize();
-  });
+const video = document.querySelector("video");
+if (window.matchMedia('(prefers-reduced-motion)').matches) {
+  video.removeAttribute("autoplay");
+  video.pause();
+}
+
+var burger = document.querySelector('.burger');
+var nav = document.querySelector('#' + burger.dataset.target);
+var header = document.querySelector('.headroom');
+
+//Bulma responsive nav
+burger.addEventListener('click', function(){
+  burger.classList.toggle('is-active');
+  nav.classList.toggle('is-active');
+});
+
+//Headroom (using Animate.css styles)
+var headroom = new Headroom(header, {
+  offset: 105,
+  tolerance: 5,
+  classes: {
+    initial: "animated",
+    pinned: "slideInDown",//custom: slideDown
+    unpinned: "slideOutUp"//custom: slideUp
+  }
+});
+headroom.init();
